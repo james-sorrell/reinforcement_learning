fc89b771554141834e64146cb9fb5454a8bbf5ab
james_sorrell <james.sorrell@droneshield.com>
2020-05-30 20:57:16 +1000

    Added tensorflow implementation of the hillclimb -> policy gradient solution

--- mountaincart_continuous/pg_hillclimb_tf.py
+++ mountaincart_continuous/pg_hillclimb_tf.py
@@ -3,8 +3,9 @@ import os
 import sys
 import config
 import numpy as np 
-import theano
-import theano.tensor as T
+import tensorflow.compat.v1 as tf
+tf.compat.v1.disable_eager_execution()
+
 import matplotlib.pyplot as plt
 from gym import wrappers
 from mountaincart.q_learning_rbf import FeatureTransformer, plot_cost_to_go
@@ -21,32 +22,36 @@ from mountaincart.q_learning_rbf import FeatureTransformer, plot_cost_to_go
 # hence favouring a bang-bang strategy.
 
 class HiddenLayer:
-  def __init__(self, M1, M2, f=T.nnet.relu, use_bias=True, zeros=False):
+  def __init__(self, M1, M2, f=tf.nn.tanh, use_bias=True, zeros=False):
     if zeros:
-      W = np.zeros((M1, M2))
+      W = np.zeros((M1, M2)).astype(np.float32)
+      self.W = tf.Variable(W)
     else:
-      W = np.random.randn(M1, M2) * np.sqrt(2 / M1)
-    self.W = theano.shared(W)
+      self.W = tf.Variable(tf.random_normal(shape=(M1, M2)))
+
     self.params = [self.W]
+
     self.use_bias = use_bias
     if use_bias:
-      self.b = theano.shared(np.zeros(M2))
-      self.params += [self.b]
+      self.b = tf.Variable(np.zeros(M2).astype(np.float32))
+      self.params.append(self.b)
     self.f = f
 
   def forward(self, X):
     if self.use_bias:
-      a = X.dot(self.W) + self.b
+      a = tf.matmul(X, self.W) + self.b
     else:
-      a = X.dot(self.W)
+      a = tf.matmul(X, self.W)
     return self.f(a)
 
 
+# approximates pi(a | s)
 class PolicyModel:
   def __init__(self, ft, D, hidden_layer_sizes_mean=[], hidden_layer_sizes_var=[]):
+
     # save inputs for copy
     self.ft = ft
-    self.D = D
+    self.D = D # dimensions of feature vector
     self.hidden_layer_sizes_mean = hidden_layer_sizes_mean
     self.hidden_layer_sizes_var = hidden_layer_sizes_var
     
@@ -58,7 +63,7 @@ class PolicyModel:
       self.mean_layers.append(layer)
       M1 = M2
 
-    # final layer
+    # final layer, 1 ensures we recieve scalar
     layer = HiddenLayer(M1, 1, lambda x: x, use_bias=False, zeros=True)
     self.mean_layers.append(layer)
 
@@ -71,80 +76,99 @@ class PolicyModel:
       M1 = M2
 
     # final layer -> Variance needs to be greater than zero,
-    # hence we use softplus as the activation.
-    layer = HiddenLayer(M1, 1, T.nnet.softplus, use_bias=False, zeros=False)
+    # hence we use softplus as the activation, 1 ensures we recieve scalar
+    layer = HiddenLayer(M1, 1, tf.nn.softplus, use_bias=False, zeros=False)
     self.var_layers.append(layer)
 
-    # get all params for gradient later
-    params = []
+    # gather params
+    self.params = []
     for layer in (self.mean_layers + self.var_layers):
-      params += layer.params
-    self.params = params
+      self.params += layer.params
 
     # inputs and targets
-    X = T.matrix('X')
-    #actions = T.vector('actions')
-    #advantages = T.vector('advantages')
+    self.X = tf.placeholder(tf.float32, shape=(None, D), name='X')
+    self.actions = tf.placeholder(tf.float32, shape=(None,), name='actions')
+    self.advantages = tf.placeholder(tf.float32, shape=(None,), name='advantages')
 
-    # calculate output and cost
     def get_output(layers):
-      Z = X
+      Z = self.X
       for layer in layers:
         Z = layer.forward(Z)
-      return Z.flatten()
+      return tf.reshape(Z, [-1])
 
+    # calculate output and cost
     mean = get_output(self.mean_layers)
-    var = get_output(self.var_layers) + 1e-4 # smoothing
+    std = get_output(self.var_layers) + 1e-4 # smoothing
 
-    # alternatively, we could create a RandomStream and sample from
-    # the Gaussian using Theano code
-    self.predict_op = theano.function(
-      inputs=[X],
-      outputs=[mean, var],
-      allow_input_downcast=True
-    )
+    # note: the 'variance' is actually standard deviation
+    # sampling from distribution + clipping
+    norm = tf.distributions.Normal(mean, std)
+    self.predict_op = tf.clip_by_value(norm.sample(), -1, 1)
+
+  def set_session(self, session):
+    self.session = session
+
+  def init_vars(self):
+    init_op = tf.variables_initializer(self.params)
+    self.session.run(init_op)
+
+  # def partial_fit(self, X, actions, advantages):
+  #   X = np.atleast_2d(X)
+  #   X = self.ft.transform(X)
+    
+  #   actions = np.atleast_1d(actions)
+  #   advantages = np.atleast_1d(advantages)
+  #   self.session.run(
+  #     self.train_op,
+  #     feed_dict={
+  #       self.X: X,
+  #       self.actions: actions,
+  #       self.advantages: advantages,
+  #     }
+  #   )
 
   def predict(self, X):
     X = np.atleast_2d(X)
     X = self.ft.transform(X)
-    return self.predict_op(X)
+    
+    return self.session.run(self.predict_op, feed_dict={self.X: X})
 
-  # You can see below that given a state input X we use the regression model
-  # to predict a mean and variance, which when sampled from gives us a distrubtion
-  # from which we sample our action, this action is then clipped such that it
-  # falls within acceptable environment parameters.
   def sample_action(self, X):
-    pred = self.predict(X)
-    mu = pred[0][0]
-    v = pred[1][0]
-    a = np.random.randn()*np.sqrt(v) + mu
-    # Environment only allows actions between -1 and 1
-    return min(max(a, -1), 1)
+    p = self.predict(X)[0]
+    # print("action:", p)
+    return p
 
-  # Hill Climbing Features 
   def copy(self):
     clone = PolicyModel(self.ft, self.D, self.hidden_layer_sizes_mean, self.hidden_layer_sizes_mean)
+    clone.set_session(self.session)
+    clone.init_vars() # tf will complain if we don't do this
     clone.copy_from(self)
     return clone
 
   def copy_from(self, other):
-    # self is being copied from other
-    for p, q in zip(self.params, other.params):
-      v = q.get_value()
-      p.set_value(v)
+    # collect all the ops
+    ops = []
+    my_params = self.params
+    other_params = other.params
+    for p, q in zip(my_params, other_params):
+      actual = self.session.run(q)
+      op = p.assign(actual)
+      ops.append(op)
+    # now run them all
+    self.session.run(ops)
 
   def perturb_params(self):
-    # For each parameter, we have a probability that we
-    # will keep the parameter and add some noise to it,
-    # otherwise, we reset the parameter.
+    ops = []
     for p in self.params:
-      v = p.get_value()
+      v = self.session.run(p)
       noise = np.random.randn(*v.shape) / np.sqrt(v.shape[0]) * 5.0
       if np.random.random() < 0.1:
         # with probability 0.1 start completely from scratch
-        p.set_value(noise)
+        op = p.assign(noise)
       else:
-        p.set_value(v + noise)
+        op = p.assign(v + noise)
+      ops.append(op)
+    self.session.run(ops)
 
 
 def play_one(env, pmodel, gamma):
@@ -209,6 +233,9 @@ def main():
   # Here we can see that we are paramaterising the policy model
   # according to the dimensions of the feature vector.
   pmodel = PolicyModel(ft, D, [], [])
+  session = tf.InteractiveSession()
+  pmodel.set_session(session)
+  pmodel.init_vars()
   gamma = 0.99
 
   record = False
