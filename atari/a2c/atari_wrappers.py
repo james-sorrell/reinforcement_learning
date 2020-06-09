f577ac2f96650a3b7c556368cb6b49c5469fc64e
James <jamessorrell95@gmail.com>
2020-06-09 20:00:55 +1000

    Removed dead code.

--- atari/a2c/atari_wrappers.py
+++ atari/a2c/atari_wrappers.py
@@ -131,18 +131,6 @@ class ClipRewardEnv(gym.RewardWrapper):
         return np.sign(reward)
 
 
-# class WarpFrame(gym.ObservationWrapper):
-#     def __init__(self, env):
-#         """Warp frames to 84x84 as done in the Nature paper and later work."""
-#         gym.ObservationWrapper.__init__(self, env)
-#         self.width = 84
-#         self.height = 84
-#         self.observation_space = spaces.Box(low=0, high=255, shape=(self.height, self.width, 1))
-
-#     def _observation(self, frame):
-#         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-#         frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
-#         return frame[:, :, None]
 class WarpFrame(gym.ObservationWrapper):
     def __init__(self, env, width=84, height=84, grayscale=True):
         """Warp frames to 84x84 as done in the Nature paper and later work."""
