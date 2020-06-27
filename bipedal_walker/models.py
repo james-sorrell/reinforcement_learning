f599513498a3da6383e7f71fb3c96fe81f4fc990
james-sorrell <jamessorrell95@gmail.com>
2020-06-28 01:02:25 +1000

    TD3 Implementation, currently running on Bi-pedal worker, seems to be converging quite well.

--- bipedal_walker/models.py
+++ bipedal_walker/models.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class TD3CriticNetwork(nn.Module):
     def __init__(self, name, beta, input_dims, fc1_dims, fc2_dims, num_actions, checkpoint_dir="tmp/td3"):
-        super(TD3CriticNetwork, self.__init())
+        super(TD3CriticNetwork, self).__init__()
         self.checkpoint_dir = checkpoint_dir
         if not os.path.exists(checkpoint_dir):
             os.makedirs(checkpoint_dir)
@@ -16,9 +16,9 @@ class TD3CriticNetwork(nn.Module):
 
     # Designed for one dimensional state space representations
     def createModel(self, input_dims, beta, fc1_dims, fc2_dims, num_actions):
-        self.fc1 = nn.Linear(input_dims[0] + num_actions, self.fc1_dims)
-        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-        self.q1 = nn.Linear(self.fc2_dims, 1)
+        self.fc1 = nn.Linear(input_dims[0] + num_actions, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        self.q1 = nn.Linear(fc2_dims, 1)
         self.optimizer = optim.Adam(self.parameters(), lr=beta)
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
@@ -40,24 +40,24 @@ class TD3CriticNetwork(nn.Module):
 
 class TD3ActorNetwork(nn.Module):
     def __init__(self, name, alpha, input_dims, fc1_dims, fc2_dims, num_actions, checkpoint_dir="tmp/td3"):
-        super(TD3ActorNetwork, self.__init())
+        super(TD3ActorNetwork, self).__init__()
         self.checkpoint_dir = checkpoint_dir
         if not os.path.exists(checkpoint_dir):
             os.makedirs(checkpoint_dir)
         self.checkpoint_file = os.path.join(self.checkpoint_dir, name)
-        self.createModel(input_dims, beta, fc1_dims, fc2_dims, num_actions)
+        self.createModel(input_dims, alpha, fc1_dims, fc2_dims, num_actions)
         
-    def createModel(self, input_dims, beta, fc1_dims, fc2_dims, num_actions):
-        self.fc1 = nn.Linear(*input_dims, self.fc1_dims)
-        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+    def createModel(self, input_dims, alpha, fc1_dims, fc2_dims, num_actions):
+        self.fc1 = nn.Linear(*input_dims, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
         # 4 continuous components of the bipedal walker actions
-        self.a1 = nn.Linear(self.fc2_dims, self.num_actions)
-        self.optimizer = optim.Adam(self.parameters(), lr=beta)
+        self.a1 = nn.Linear(fc2_dims, num_actions)
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
-    def forward(self, state, action):
-        fc1 = F.relu(self.fc1(state, dim=1))
+    def forward(self, state):
+        fc1 = F.relu(self.fc1(state))
         fc2 = F.relu(self.fc2(fc1))
         # Here we are activating the output of the action layer
         # binding it to +-1
